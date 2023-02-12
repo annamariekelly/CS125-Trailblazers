123d863c8b7ed5edf22755b16058639a5864449c
Preston <yaumanyoung@yahoo.com>
2023-02-11 21:10:24 -0800

    Finished supabase function for getting and adding trips for a user

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import { fetchUser, createUser, updateUser, deleteUser } from "./helpers/supabase";
+import {
+  fetchAllUsers,
+  fetchUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  getTrips,
+  addTrip,
+} from "./helpers/supabase";
 
 function App() {
   const [data, setData] = React.useState(null);
@@ -13,35 +21,67 @@ function App() {
     //     .then((res) => res.json())
     //     .then((data) => setData(data.message));
 
+    fetchAllUsers().then(({data, error}) => {
+      if (data) {
+        console.log('all users: ', data);
+      } else {
+        console.log('all users: ', data);
+        console.log('fetch user error: ', error.message);
+      }
+    });
+
     fetchUser(7654321)
-        .then((data) => {
-          if (data) {
-            setData(data[0].student_id);
-          } else {
-            console.log('fetch user error: ', data.message);
-          }
-        });
+      .then(({data, error}) => {
+        if (data) {
+          console.log('one user: ', data);
+          setData(data[0].student_id);
+        } else {
+          console.log('fetch user error: ', error.message);
+        }
+      });
 
     createUser(7654321, 'Anna password', 'gym')
-        .then((err) => {
-          if (err) {
-            console.log('create user error: ', err.message);
-          }
-        });
+      .then((err) => {
+        if (err) {
+          console.log('create user error: ', err.message);
+        }
+      });
 
     updateUser(7654321, 'park')
-        .then((err) => {
-          if (err) {
-            console.log('update user error: ', err.message);
-          }
-        });
+      .then((err) => {
+        if (err) {
+          console.log('update user error: ', err.message);
+        }
+      });
 
     deleteUser(7654321, 'park')
-        .then((err) => {
-          if (err) {
-            console.log('delete user error: ', err.message);
-          }
-        });
+      .then((err) => {
+        if (err) {
+          console.log('delete user error: ', err.message);
+        }
+      });
+
+    getTrips('Past', 43798917).then(({data, error}) => {
+        if (data) {
+          console.log('all past trips: ', data);
+        } else {
+          console.log('past trips error: ', error.message);
+        }
+      });
+
+    addTrip('Past', 43798917, 2, 4)
+      .then((err) => {
+        if (err) {
+          console.log('past trip error: ', err.message);
+        }
+      });
+
+    addTrip('Past', 43798917, 1, 4)
+      .then((err) => {
+        if (err) {
+          console.log('past trip error: ', err.message);
+        }
+      });
 
   }, []);
 
--- client/src/helpers/supabase.js
+++ client/src/helpers/supabase.js
@@ -8,6 +8,15 @@ const supabase = createClient(
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1d294b2pxemxvZ2p2bmNsZXhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYxNTk3NDQsImV4cCI6MTk5MTczNTc0NH0.HoWjtoTQ_d2q9ExpKEObL531Q7vWINaqsQBxwDgVhxo',
     );
 
+// Fetches all users.
+export const fetchAllUsers = async () => {
+    const { data, error } = await supabase
+        .from('User')
+        .select();
+
+    return {data, error};
+}
+
 // Fetches a user by student id.
 export const fetchUser = async (student_id) => {
     const { data, error } = await supabase
@@ -15,15 +24,7 @@ export const fetchUser = async (student_id) => {
         .select()
         .eq('student_id', student_id);
 
-    console.log('data: ', data);
-    console.log('error: ', error);
-
-    if (error) {
-      console.log('error: ', error.message);
-      return null;
-    }
-
-    return data;
+    return {data, error};
 }
 
 // Creates a new user.
@@ -55,6 +56,31 @@ export const deleteUser = async (student_id) => {
         .from('User')
         .delete()
         .eq('student_id', student_id);
+
+    if (error) {
+        return error;
+    }
+}
+
+// Gets all trips for a user. Works for past and saved trips.
+export const getTrips = async (trip_type, student_id) => {
+    const { data, error } = await supabase
+        .from(`${trip_type}_Trips`)
+        .select()
+        .eq('student_id', student_id);
+
+    return {data, error};
+}
+
+// Adds a trip for a user. Works for past and saved trips.
+export const addTrip = async (trip_type, student_id, business_id, rating) => {
+    const { error } = await supabase
+        .from(`${trip_type}_Trips`)
+        .insert({
+            student_id,
+            trip_id: business_id,
+            rating,
+        });
 
     if (error) {
         return error;
