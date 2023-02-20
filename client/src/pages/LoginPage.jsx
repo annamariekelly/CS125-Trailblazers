a92fe4845e19b1e64d04c9d6c875755ac7b0ee3b
Preston <yaumanyoung@yahoo.com>
2023-02-20 11:15:20 -0800

    Finished basic login functionality and added show password button

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,12 +1,39 @@
 import React, { useState } from "react";
 
+import { fetchUser } from "../helpers/supabase";
+
 const LoginForm = () => {
     const [studentId, setStudentId] = useState('');
     const [password, setPassword] = useState('');
 
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleShowPassword = (event) => {
+        event.preventDefault();
+        setShowPassword(!showPassword);
+    }
+
     const handleLoginSubmit = (event) => {
         event.preventDefault();
         alert(`Student ID: ${studentId}; Password: ${password}`);
+
+        fetchUser(studentId)
+            .then(({data, error}) => {
+                if (data) {
+                    console.log('one user: ', data);
+
+                    if (data.length === 0) {
+                        alert(`Login unsuccessful: No User with ID ${studentId} found!`);
+                    } else if (data[0].password === password) {
+                        alert('Login successful!');
+                    } else {
+                        alert('Login unsuccessful: Wrong Password!');
+                    }
+                } else {
+                    console.log(`fetch user error: ${error.message}`);
+                    alert(`Login unsuccessful (database error): ${error.message}`);
+                }
+            });
     };
 
     return (
@@ -23,11 +50,14 @@ const LoginForm = () => {
           <div>
             <label>Password:
                 <input 
-                type="text" 
+                type={showPassword ? "text" : "password"} 
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 />
             </label>
+            <button onClick={handleShowPassword}>
+                Show
+            </button>
           </div>
           <input type="submit" />
         </form>
