e6c4503a32c95ec2e928c25d82ab7daeb36d7fb9
E3x3 <roush.ethan@gmail.com>
2023-02-28 17:25:02 -0800

    Added probability functionality for Recommendation Algorithm

--- client/src/backend/recAlgorithm.js
+++ client/src/backend/recAlgorithm.js
@@ -1,4 +1,5 @@
 import {findResults, sortResults} from "./yelpDataProcessor.js"
+import {addTrip, getTrips} from '../database/supabase.js'
 
 //Maps intensity to corresponding speed in meters/second
 const INTENSITIES = {
@@ -11,19 +12,20 @@ const INTENSITIES = {
 /**
  * Recommends a list of desired locations based on the user's location, terrain preference, desired maximum length, and intensity.
  *
+ * @param {number} studentID - Student ID
  * @param {Object} currentLocation - The user's current location as an object with latitude and longitude properties.
  * @param {number} terrainPreference - The user's preferred type of walk. Check yelpDataProcessor.js for indexes and their corresponding meanings.
  * @param {number} maxTime - The user's desired maximum walk time in minutes
  * @param {number} intensity - The user's desired intensity level.
  * @returns {Promise} A promise that resolves to an array of location recommendations
  */
-export async function getRecList(currentLocation, terrainPreference, maxTime, intensity) {
+export async function getRecList(studentID, currentLocation, terrainPreference, maxTime, intensity) {
     const searchRadius = calcSearchRadius(maxTime, intensity);
     let results = await findResults(currentLocation, terrainPreference, searchRadius); //add current location and search radius
 
     //pre-processing
     let sortedResults = sortResults(results, "d");
-    let trimmedResults = trimResults(sortedResults);
+    let trimmedResults = trimResults(studentID, sortedResults);
 
     return trimmedResults;
 }
@@ -44,10 +46,11 @@ function calcSearchRadius(maxTime, intensity= 0){
  * removes locations based on previous user rating if the user has seen
  * the location before.
  *
+ * @param studentID - Student ID
  * @param {Array} results - Yelp API Results
  * @returns {Array} Returns trimmed results list
  */
-function trimResults(results){
+function trimResults(studentID, results){
     function Location(id, name, image_url, street, city, url, distance, rating){
         this.id = id;
         this.name = name;
@@ -59,13 +62,23 @@ function trimResults(results){
         this.rating = rating;
     }
     let trimmedResults = []
-    for (const result of results){
-        if (calcChance(result)){
-            trimmedResults.push(new Location(result.id, result.name, result.image_url,
+    getTrips("Past", studentID).then(({data, error}) => {
+        if (data) {
+            console.log('Request for Past Trips succeeded with data: ', data);
+            for (const result of results){
+                if (calcChance(result, data)){
+                    trimmedResults.push(new Location(result.id, result.name, result.image_url,
+                        result.location.address1, result.location.city, result.url, result.distance, result.rating));
+                }
+            }
+            return trimmedResults
+        } else {
+            console.log('Get past trips error: ', error);
+            trimmedResults = results.map(result => new Location(result.id, result.name, result.image_url,
                 result.location.address1, result.location.city, result.url, result.distance, result.rating));
+            return trimmedResults
         }
-    }
-    return trimmedResults
+    });
 }
 
 
@@ -73,14 +86,36 @@ function trimResults(results){
  * Calculates the chance an individual location will stay on the recommendation list.
  *
  * @param {Object} location - Individual location
+ * @param {Array} pastTrips - Past trips the user has taken
  * @returns {number} Returns 1 if the RNG has determined the location will stay
  * on the recommendation list. 0 otherwise.
  */
-function calcChance(location){
-    return 1;
+function calcChance(location, pastTrips){
+    let rating = 5;
+    for (let trip of pastTrips){
+        if (trip.business_id === location.id) {
+            if (trip.rating != null) {rating = trip.rating;}
+        }
+    }
+
+    const threshold = Math.random();
+
+    if (threshold < rating/5) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 
 //TESTING
-//let recList = getRecList('3641 Baylor Street, Irvine, CA', 0, 30, 0);
+/*
+addTrip('Past', 43798917, 'UQdf0dhCdH-IpNhCseoibQ', 1)
+    .then((err) => {
+        if (err) {
+            console.log('add past trip error: ', err.message);
+        }
+    });
+*/
+//let recList = getRecList(43798917,'3641 Baylor Street, Irvine, CA', 0, 30, 0);
 //console.log(recList);
