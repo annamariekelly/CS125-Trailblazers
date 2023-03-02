0cc81af7db53300aef99a912fa92368a49c42c57
Preston <yaumanyoung@yahoo.com>
2023-03-01 22:45:30 -0800

    Mapping user place category to enum

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -14,7 +14,7 @@ const HomePage = () => {
     const navigate = useNavigate();
     const location = useLocation();
     const { student_id } = location.state;
-    const [terrainType, setTerrainType] = useState(0);
+    const [filterTerrainType, setFilterTerrainType] = useState(0);
     const [intensity, setIntensity] = useState(0);
     const [time, setTime] = useState(TIMES[0]);
     
@@ -23,17 +23,25 @@ const HomePage = () => {
             .then(({data, error}) => {
                 if (data) {
                     console.log('one user: ', data);
+
+                    const { place_category } = data[0];
+                    console.log(`place cat: ${place_category}`);
+
+                    const place_category_enum = TERRAIN_TYPES.findIndex((terrain_type) => place_category === terrain_type);
+                    console.log(`place cat enum: ${place_category_enum}`);
+
+                    // getRecList(student_id, '3641 Baylor Street, Irvine, CA', place_category_enum, time, intensity)
+                    //     .then((rec_list) => console.log(`Rec List: ${rec_list}`));
                 } else {
                     console.log('fetch user error: ', error.message);
                 }
             });
 
-        // getRecList(student_id, '3641 Baylor Street, Irvine, CA', terrainType, time, intensity)
-        //     .then((rec_list) => console.log(`Rec List: ${rec_list}`));
+        
     }, []);
 
     const handleTerrainDropdownChange = (event) => {
-        setTerrainType(event.target.value);
+        setFilterTerrainType(event.target.value);
     }
 
     const handleIntensityDropdownChange = (event) => {
@@ -62,7 +70,7 @@ const HomePage = () => {
             </button>
             <div>
                 <Dropdown label="Terrain:" items={TERRAIN_TYPES} handleChange={handleTerrainDropdownChange} isEnum/>
-                <p>Terrain Type Chosen: {terrainType}</p>
+                <p>Terrain Type Chosen: {filterTerrainType}</p>
             </div>
             <div>
                 <Dropdown label="Intensity:" items={INTENSITIES} handleChange={handleIntensityDropdownChange} isEnum/>
