342fe6fc569f27bd6b94f438ca5eca28ceead89a
Lee <lbahir@uci.edu>
2023-03-10 11:09:39 -0800

    cleaned up for merging without getbusinessinfo

--- client/src/components/TripCard.jsx
+++ client/src/components/TripCard.jsx
@@ -37,7 +37,6 @@ const Card = ({name, uRating, yRating, dist, img, url, street, city}) => {
 }
 
 const TripCard = ({student_id, page}) => {
-    // const [trips, setTrips] = useState([]);
     const [businessInfoList, setbusinessInfoList] = useState([]);
     
     useEffect(() => {
@@ -45,7 +44,6 @@ const TripCard = ({student_id, page}) => {
             .then(({data, error}) => {
                 if (data) {
                     console.log('trips returned: ', data);
-                    // setTrips(data); 
 
                     let trips = data;
 
@@ -53,8 +51,8 @@ const TripCard = ({student_id, page}) => {
                     for (let i = 0; i < Object.keys(trips).length; i++){
                         let trip = trips[i];
                         
-                        // temporary dummy business info:
-                        let businessInfo = {  // ACTUALLY CALL GET BUSINESS INFO FOR TRIP
+                        // ACTUALLY CALL GET BUSINESS INFO FOR TRIP
+                        let businessInfo = {  
                             id: '7z9LRl5rLzSc7I9nSYDnnA',
                             name: 'Cheesetella',
                             image_url: 'https://s3-media3.fl.yelpcdn.com/bphoto/f7ExT6WliNxZ2q0v-IuCOg/o.jpg',
@@ -63,95 +61,19 @@ const TripCard = ({student_id, page}) => {
                             url: 'https://www.yelp.com/biz/cheesetella-irvine?adjust_creative=67rtU44aHwVN8WeBY1zCbg&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=67rtU44aHwVN8WeBY1zCbg',
                             distance: 611.654965783868,
                             rating: 4,
-                            userRating: trip.rating // MANUALLY ADDED
+                            // MANUALLY ADDED
+                            userRating: trip.rating 
                         }
 
                         businessInfoUpdater.push(businessInfo);
-                        console.log('updated bus info = ', businessInfoUpdater);  
                     }
-
-                    // //////////////////// TEMP NON ASYNC DEMO
-                    // let businessInfo = { 
-                    //     id: '7z9LRl5rLzSc7I9nSYDnnA',
-                    //     name: 'Cheesetella',
-                    //     image_url: 'https://s3-media3.fl.yelpcdn.com/bphoto/f7ExT6WliNxZ2q0v-IuCOg/o.jpg',
-                    //     street: '17655 Harvard Ave',
-                    //     city: 'Irvine',
-                    //     url: 'https://www.yelp.com/biz/cheesetella-irvine?adjust_creative=67rtU44aHwVN8WeBY1zCbg&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=67rtU44aHwVN8WeBY1zCbg',
-                    //     distance: 611.654965783868,
-                    //     rating: 4,
-                    // }
-
-                    // businessInfo.uRating = 5; // trip.rating;
-                    // businessInfoUpdater.push(businessInfo);
-                    // ///////////////////////
-
                     setbusinessInfoList(businessInfoUpdater);
                 
-
-
-
                 } else {
                     console.log('fetch user error: ', error.message);
                 }
             });
     }, []);
-
-
-
-    // useEffect(() => {     
-    //     console.log('trips useeffect ', trips);  
-    //     let businessInfoUpdater = [];
-
-    //     // for (let i = 0; i < trips.size; i++){
-    //     //     let trip = trips[i];
-
-    //     //     // temporary dummy business info:
-    //     //     let businessInfo = [{  // ACTUALLY CALL GET BUSINESS INFO FOR TRIP
-    //     //         id: '7z9LRl5rLzSc7I9nSYDnnA',
-    //     //         name: 'Cheesetella',
-    //     //         image_url: 'https://s3-media3.fl.yelpcdn.com/bphoto/f7ExT6WliNxZ2q0v-IuCOg/o.jpg',
-    //     //         street: '17655 Harvard Ave',
-    //     //         city: 'Irvine',
-    //     //         url: 'https://www.yelp.com/biz/cheesetella-irvine?adjust_creative=67rtU44aHwVN8WeBY1zCbg&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=67rtU44aHwVN8WeBY1zCbg',
-    //     //         distance: 611.654965783868,
-    //     //         rating: 4,
-    //     //     }]
-
-    //     //     businessInfo.uRating = trip.rating;
-    //     //     businessInfoUpdater.push(businessInfo);
-    //     //     console.log('updated bus info = ', businessInfoUpdater);  
-    //     // }
-
-    //     //////////////////// TEMP NON ASYNC DEMO
-    //     let businessInfo = { 
-    //         id: '7z9LRl5rLzSc7I9nSYDnnA',
-    //         name: 'Cheesetella',
-    //         image_url: 'https://s3-media3.fl.yelpcdn.com/bphoto/f7ExT6WliNxZ2q0v-IuCOg/o.jpg',
-    //         street: '17655 Harvard Ave',
-    //         city: 'Irvine',
-    //         url: 'https://www.yelp.com/biz/cheesetella-irvine?adjust_creative=67rtU44aHwVN8WeBY1zCbg&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=67rtU44aHwVN8WeBY1zCbg',
-    //         distance: 611.654965783868,
-    //         rating: 4,
-    //     }
-
-    //     businessInfo.uRating = 5; // trip.rating;
-    //     businessInfoUpdater.push(businessInfo);
-    //     businessInfoUpdater.push(businessInfo);
-    //     ///////////////////////
-
-    //     setbusinessInfoList(businessInfoUpdater);
-
-    //     // getBusinessInfo('o6ziktvJIlqumDpggkS-dQ')
-    //     //     .then(({data}) => {
-    //     //         if (data) {
-    //     //             console.log('business info: ', data);
-    //     //         } else {
-    //     //             console.log('fetch user error');
-    //     //         }
-    //     //     }); name, uRating, yRating, dist, img, url, street, city
-        
-    // }, []);
 
     return (
         <div>
@@ -165,7 +87,8 @@ const TripCard = ({student_id, page}) => {
                                 img={bI.image_url}
                                 url={bI.url} 
                                 street={bI.street} 
-                                city={bI.city}/>
+                                city={bI.city}
+                        />
                     </div>
                 );
             } ) 
